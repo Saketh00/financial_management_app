d6cd905e707db960edcc56444ec18e193c6bbd22
Saketh00 <73815156+Saketh00@users.noreply.github.com>
2021-12-28 19:59:07 +0530

    Initial commit

---
+++ lib/views/Listexpenses.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ListExpenses extends StatelessWidget{
+  const ListExpenses({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+  
+}
