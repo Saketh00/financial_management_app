e8f2642e34237916e51c16bc38c6a12e8430489f
Saketh00 <73815156+Saketh00@users.noreply.github.com>
2021-12-29 14:07:55 +0530

    Update Listexpenses.dart

--- lib/views/Listexpenses.dart
+++ lib/views/Listexpenses.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListExpenses extends StatelessWidget{
+class ListExpenses extends StatelessWidget {
   const ListExpenses({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              SizedBox(height: 25,),
+              Container(
+                //color: Colors.green,
+                alignment: Alignment.center,
+                child: Text('Expenses List',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+              ),
+              //SizedBox(height: 5000,),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          print('add');
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
-  
 }
